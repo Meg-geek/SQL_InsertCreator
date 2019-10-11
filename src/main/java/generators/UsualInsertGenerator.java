efd56e0f626b9c48b8a55a35eb3b73d787319049
Meg-geek <m.khlybova@g.nsu.ru>
2019-10-11 19:02:50 +0700

    UsualInsertGenerator first version

--- src/main/java/generators/UsualInsertGenerator.java
+++ src/main/java/generators/UsualInsertGenerator.java
@@ -10,48 +10,49 @@ public class UsualInsertGenerator implements InsertGenerator {
         }
         List<String> columns = parameters[1];
         List <String> values = parameters[2];
-        StringBuilder sqlInsertBuilder = new StringBuilder("INSERT INTO " + parameters[0].get(0));
-        sqlInsertBuilder.append(toInsertForm(columns) + "VALUES" + System.lineSeparator());
-        sqlInsertBuilder.append(toValuesForm(values, columns.size()) + ";");
+        StringBuilder sqlInsertBuilder = new StringBuilder
+                ("INSERT INTO " + parameters[0].get(0) + System.lineSeparator());
+        sqlInsertBuilder.append(toInsertForm(columns));
+        sqlInsertBuilder.append("VALUES");
+        sqlInsertBuilder.append(System.lineSeparator());
+        sqlInsertBuilder.append(toValuesForm(values, columns.size()));
+        sqlInsertBuilder.append(";");
         return sqlInsertBuilder.toString();
     }
 
     private String toValuesForm(List<String> values, int columnsAmount){
         StringBuilder valuesBuilder = new StringBuilder();
-        for(int i = 0; i < values.size() - columnsAmount * 2; i += columnsAmount){
+        for(int i = 0; i < values.size() - columnsAmount; i += columnsAmount){
             valuesBuilder.append(toInsertForm(values.subList(i, i + columnsAmount)) +
                     "," + System.lineSeparator());
         }
-        valuesBuilder.append(toInsertForm(values.subList(values.size() - columnsAmount, values.size()));
+        valuesBuilder.append(toInsertForm(values.subList(values.size() - columnsAmount, values.size())));
         return valuesBuilder.toString();
     }
 
     private String toInsertForm(List <String> parameters){
-        StringBuilder insertFormBuilder = new StringBuilder("(");
+        StringBuilder insertFormBuilder = new StringBuilder("( ");
         for(int i = 0; i < parameters.size() - 1; i++){
             insertFormBuilder.append(parameters.get(i) + ", ");
         }
-        insertFormBuilder.append(parameters.get(parameters.size() - 1) + " ) ");
+        insertFormBuilder.append(parameters.get(parameters.size() - 1) + " ) "
+        + System.lineSeparator());
         return insertFormBuilder.toString();
     }
 
     private boolean checkParameters(List<String>... param){
         if(param == null){
-            System.out.println("null");
             return false;
         }
         if(param.length < 3){
-            System.out.println("length");
             return false;
         }
         for(List<String> paramList : param){
             if(paramList.size() < 1){
-                System.out.println("size");
                 return false;
             }
         }
         if(param[2].size() % param[1].size() != 0){
-            System.out.println("column, val ");
             return false;
         }
         return true;
