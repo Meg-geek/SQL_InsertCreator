442ecca96f7ea7ff476f0d49361cc5bb4816aed0
Meg-geek <m.khlybova@g.nsu.ru>
2019-10-13 12:18:22 +0700

    StringBuilder small fixes

--- src/main/java/generators/UsualInsertGenerator.java
+++ src/main/java/generators/UsualInsertGenerator.java
@@ -3,6 +3,7 @@ package generators;
 import java.util.List;
 
 public class UsualInsertGenerator implements InsertGenerator {
+
     @Override
     public String createInsertSQLCommand(List<String>... parameters) {
         if(!checkParameters(parameters)){
@@ -13,18 +14,21 @@ public class UsualInsertGenerator implements InsertGenerator {
         StringBuilder sqlInsertBuilder = new StringBuilder
                 ("INSERT INTO " + parameters[0].get(0) + System.lineSeparator());
         sqlInsertBuilder.append(toInsertForm(columns));
+        sqlInsertBuilder.append(System.lineSeparator());
         sqlInsertBuilder.append("VALUES");
         sqlInsertBuilder.append(System.lineSeparator());
         sqlInsertBuilder.append(toValuesForm(values, columns.size()));
         sqlInsertBuilder.append(";");
+        sqlInsertBuilder.append(System.lineSeparator());
         return sqlInsertBuilder.toString();
     }
 
     private String toValuesForm(List<String> values, int columnsAmount){
         StringBuilder valuesBuilder = new StringBuilder();
         for(int i = 0; i < values.size() - columnsAmount; i += columnsAmount){
-            valuesBuilder.append(toInsertForm(values.subList(i, i + columnsAmount)) +
-                    "," + System.lineSeparator());
+            valuesBuilder.append(toInsertForm(values.subList(i, i + columnsAmount)));
+            valuesBuilder.append(",");
+            valuesBuilder.append(System.lineSeparator());
         }
         valuesBuilder.append(toInsertForm(values.subList(values.size() - columnsAmount, values.size())));
         return valuesBuilder.toString();
@@ -33,10 +37,11 @@ public class UsualInsertGenerator implements InsertGenerator {
     private String toInsertForm(List <String> parameters){
         StringBuilder insertFormBuilder = new StringBuilder("( ");
         for(int i = 0; i < parameters.size() - 1; i++){
-            insertFormBuilder.append(parameters.get(i) + ", ");
+            insertFormBuilder.append(parameters.get(i));
+            insertFormBuilder.append(", ");
         }
-        insertFormBuilder.append(parameters.get(parameters.size() - 1) + " ) "
-        + System.lineSeparator());
+        insertFormBuilder.append(parameters.get(parameters.size() - 1));
+        insertFormBuilder.append(" )");
         return insertFormBuilder.toString();
     }
 
@@ -52,9 +57,6 @@ public class UsualInsertGenerator implements InsertGenerator {
                 return false;
             }
         }
-        if(param[2].size() % param[1].size() != 0){
-            return false;
-        }
-        return true;
+        return (param[2].size() % param[1].size() == 0);
     }
 }
